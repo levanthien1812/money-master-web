a9ade83b2cdb0ac24feb4fcf4831f4729e722da9
Le Van Thien <20521947@gm.uit.edu.vn>
2024-07-12 05:39:13 +0700

    fix fetch wallets and set choosenWallet

--- src/components/routes/ProtectedRoute.jsx
+++ src/components/routes/ProtectedRoute.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router";
-import { fetchWallets } from "../../stores/wallets";
+import { fetchWallets, walletActions } from "../../stores/wallets";
 import { useDispatch, useSelector } from "react-redux";
 
 function ProtectedRoute() {
   const dispatch = useDispatch();
   const { isAuthenticated, roles } = useSelector((state) => state.auth);
-  const walletChosen = useSelector((state) => state.wallet.walletChosen);
+  const { walletChosen, wallets } = useSelector((state) => state.wallet);
+
+  useEffect(() => {
+    if (isAuthenticated && roles.includes("user")) {
+      dispatch(fetchWallets()).then(({ payload }) => {
+        if (!walletChosen) {
+          const walletChosen = payload.find((wallet) => wallet.default === 1);
+          console.log("Chosen wallet:", walletChosen); // Debug log
+          dispatch(walletActions.setWalletChosen(walletChosen));
+        }
+      });
+    }
+  }, [dispatch, isAuthenticated]);
 
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />;
@@ -16,12 +28,7 @@ function ProtectedRoute() {
     return <Outlet />;
   }
 
-  dispatch(fetchWallets());
-
-  if (walletChosen) {
-    console.log(walletChosen);
-    return <Outlet />;
-  }
+  if (walletChosen) return <Outlet />;
 }
 
 export default ProtectedRoute;
